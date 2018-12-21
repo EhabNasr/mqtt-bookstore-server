45f86fbf54b5406886c2f029b3fa6c942eb68ef5
ehab <ehab.mohamed.nasr@gmail.com>
2018-12-21 21:15:40 +0200

    initial code

--- src/main/java/com/mapr/demo/mqtt/simple/Publisher.java
+++ src/main/java/com/mapr/demo/mqtt/simple/Publisher.java
@@ -14,53 +14,62 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import java.util.Scanner;
 
 public class Publisher {
+    public static boolean acknowledge = false;
 
   public static void main(String[] args) {
+    sendToRobot("r", 1);
+      sendToRobot("l", 1);
 
-      String topic        = "alfred/walk";
-    int qos             = 0;
-    String broker       = "tcp://m12.cloudmqtt.com:17492";
-    String clientId     = "234";
-    MemoryPersistence persistence = new MemoryPersistence();
-
-    try {
-        MqttConnectOptions conOpt = new MqttConnectOptions();
-        conOpt.setCleanSession(true);
-        conOpt.setUserName("qldkjxzp");
-        conOpt.setPassword("gK5_OW6wcgAd".toCharArray());
-
-        MqttClient subscriber_client=new MqttClient(broker, MqttClient.generateClientId(),new MemoryPersistence());
-        subscriber_client.setCallback( new SimpleMqttCallBack() );
-
-        MqttClient publisher_client = new MqttClient(broker, clientId, new MemoryPersistence());
-
-      System.out.println("Connecting to broker: "+broker);
-      publisher_client.connect(conOpt);
-      subscriber_client.connect(conOpt);
-      subscriber_client.subscribe(topic);
-
-        System.out.println("Connected");
-          while (true) {
-              System.out.print("Enter your command: " );
-              Scanner s = new Scanner(System.in);
-              String str = s.nextLine();
-              if(str == "e\n") break;
-              System.out.println("Publishing message: " + str);
-              MqttMessage message = new MqttMessage(str.getBytes());
-              message.setQos(qos);
-              publisher_client.publish(topic, message);
-              System.out.println("Message published");
-          }
-      publisher_client.disconnect();
-      System.out.println("Disconnected");
-      System.exit(0);
-    } catch(MqttException me) {
-      System.out.println("reason "+me.getReasonCode());
-      System.out.println("msg "+me.getMessage());
-      System.out.println("loc "+me.getLocalizedMessage());
-      System.out.println("cause "+me.getCause());
-      System.out.println("excep "+me);
-      me.printStackTrace();
-    }
   }
+
+    private static boolean sendToRobot (String move, int robot_id){
+        String topic = "alfred/walk/" + Integer.toString(robot_id);
+        String broker       = "tcp://m12.cloudmqtt.com:17492";
+        String clientId     = "234";
+        int qos             = 0;
+//        MemoryPersistence persistence = new MemoryPersistence();
+        try {
+
+            MqttConnectOptions conOpt = new MqttConnectOptions();
+            conOpt.setCleanSession(true);
+            conOpt.setUserName("qldkjxzp");
+            conOpt.setPassword("gK5_OW6wcgAd".toCharArray());
+
+            MqttClient subscriber_client=new MqttClient(broker, MqttClient.generateClientId(),new MemoryPersistence());
+            SimpleMqttCallBack myCallBack = new SimpleMqttCallBack();
+            subscriber_client.setCallback( myCallBack );
+
+            MqttClient publisher_client = new MqttClient(broker, clientId, new MemoryPersistence());
+
+            System.out.println("Connecting to broker: "+broker);
+            publisher_client.connect(conOpt);
+            subscriber_client.connect(conOpt);
+            subscriber_client.subscribe("acc/here/" + Integer.toString(robot_id));
+            System.out.println("Connected");
+            System.out.println("Publishing message: " + move);
+            MqttMessage message = new MqttMessage(move.getBytes());
+            message.setQos(qos);
+            publisher_client.publish(topic, message);
+            System.out.println("Message published");
+            while (Publisher.acknowledge == false){
+
+            }
+            Publisher.acknowledge = false;
+            System.out.println("I'm here 1");
+            publisher_client.disconnect();
+            subscriber_client.disconnect();
+
+            return true;
+
+        }catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
+
+        return false;
+    }
 }
--- src/main/java/com/mapr/demo/mqtt/simple/SimpleMqttCallBack.java
+++ src/main/java/com/mapr/demo/mqtt/simple/SimpleMqttCallBack.java
@@ -5,14 +5,15 @@ import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class SimpleMqttCallBack implements MqttCallback {
-
   public void connectionLost(Throwable throwable) {
     System.out.println("Connection to MQTT broker lost!");
   }
 
   public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
-    System.out.println("Message received:\t"+ new String(mqttMessage.getPayload()) );
-}
+    System.out.println("Message received: "+ new String(mqttMessage.getPayload()) );
+    System.out.print(">>> ");
+    Publisher.acknowledge = true;
+  }
 
   public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
   }
